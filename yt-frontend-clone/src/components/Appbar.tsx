13d7c9284d91966920fbf8678ba584d55a91a1df
coder om <mail.coderom@gmail.com>
2024-06-06 23:25:24 +0530

    Update topbar

--- yt-frontend-clone/src/components/Appbar.tsx
+++ yt-frontend-clone/src/components/Appbar.tsx
@@ -1,9 +1,14 @@
 import { SearchBar } from "./Searchbar"
 
 export const Appbar = () => {
-    return <div className="flex justify-between	pl-3 pr-3 pt-1">
+    return <div className="flex justify-between items-center py-3">
         <div>
-            <img className="w-28" src="yt.jpeg" alt="" />
+            <div className="flex justify-between gap-2 pl-5 items-center ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+                <img className="w-36" src="yt.jpeg" alt="" />
+            </div>
         </div>
         <div>
             <SearchBar />
--- yt-frontend-clone/src/components/Searchbar.tsx
+++ yt-frontend-clone/src/components/Searchbar.tsx
@@ -1,9 +1,9 @@
 export const SearchBar = () => {
     return <div className="w-2/4">
         <form>
-            <div className="w-96 flex text-gray-900 border border-gray-500 rounded-3xl p-1 pl-3 text-sm pr-2">
-                <input id="default-search" className="w-full bg-slate-950 text-white border-none outline-none" placeholder="Search" required />
-                <button className="bg-slate-950 text-gray-400 font-bold py-2 px-4 rounded inline-flex items-center">
+            <div className="w-96 flex text-gray-900  pl-3 text-sm">
+                <input id="default-search" className="w-96 p-3 border-l border-t border-b border-inherit outline-none rounded-l-3xl" placeholder="Search" required />
+                <button className="py-2 px-4 rounded-r-3xl inline-flex items-center border border-inherit outline-none bg-gray-100">
                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
