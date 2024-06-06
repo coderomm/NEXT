1aae4c5f99932d0d47de8cc9517de877ca07bb3d
coder om <mail.coderom@gmail.com>
2024-06-06 22:46:29 +0530

    Add missing TS

--- yt-frontend-clone/src/components/VideoGrid.tsx
+++ yt-frontend-clone/src/components/VideoGrid.tsx
@@ -1,5 +1,5 @@
+import { VIDEOS } from '@/videos'
 import { VideoCard } from './VideoCard'
-import { VIDEOS } from '../../videos'
 
 export const VideoGrid = () => {
     return <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4'>
---
+++ yt-frontend-clone/src/videos.ts
@@ -0,0 +1,90 @@
+export interface Video {
+    title: string,
+    description: string,
+    author: string,
+    views: string,
+    timestamp: string,
+    channelLogo: string
+    thumbnail: string,
+}
+export const VIDEOS: Video[] = [{
+    title: "How to master WEB3 in 2020 Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "10K views ",
+    timestamp: " 1 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2021 (Complete Roadmap) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "20K views ",
+    timestamp: " 2 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2022 (Complete Syllabus) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "20K views ",
+    timestamp: " 3 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2023 (Complete Guide) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "40K views ",
+    timestamp: " 4 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2024 (Complete Guide) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "50K views ",
+    timestamp: " 5 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2020 Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "10K views ",
+    timestamp: " 1 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2021 (Complete Roadmap) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "20K views ",
+    timestamp: " 2 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2022 (Complete Syllabus) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "20K views ",
+    timestamp: " 3 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2023 (Complete Guide) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "40K views ",
+    timestamp: " 4 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}, {
+    title: "How to master WEB3 in 2024 (Complete Guide) Harkirat Singh",
+    description: "This is the description for this youtube video from hkirat",
+    author: "Harkirat Singh",
+    views: "50K views ",
+    timestamp: " 5 weeks ago",
+    channelLogo: "channelLogo.jpg",
+    thumbnail: "thumbnail.jpg"
+}]
